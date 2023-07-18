fce7b3f696ab5ca36e5303bd98099be2d2cf89f3
R <67007753+badb000m@users.noreply.github.com>
2023-07-18 18:56:43 +0100

    Update Process.py

--- lib/Process.py
+++ lib/Process.py
@@ -1,1 +1,11 @@
+import multiprocessing
 
+
+class Process(multiprocessing.Process):
+    def __init__(self, **kwargs) -> None:
+        super(Process, self).__init__(**kwargs)
+        self._target = kwargs.get('target')
+
+    @property
+    def target_class(self) -> object:
+        return self._target
